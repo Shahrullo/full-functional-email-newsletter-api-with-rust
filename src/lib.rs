18632c881b8038271f1e82a26165828ad9abaa40
Shahrullo <shahrullo777@gmail.com>
2024-05-15 19:11:01 +0900

    integrated test for health_check is added and modified files

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,22 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+//! src/lib.rs
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::Server;
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 // We need to mark `run` as public.
-// It is no longer a binary entrypoint, therefore we can mark it as async
-// without having to use any proc-macro incantation.
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+// It is no longer a binary entrypoint
+// We have no .await call, so it is not needed anymore.
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+        })
+        .bind("127.0.0.1:8000")?
+        .run();
+    // No .await here!
+    Ok(server)
 }
