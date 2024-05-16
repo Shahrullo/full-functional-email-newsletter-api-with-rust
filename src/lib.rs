c024d215be630dda2fb8bb681207405b3f4cb586
Shahrullo <shahrullo777@gmail.com>
2024-05-16 20:38:25 +0900

    subscriptions route is added

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,11 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+// We always return a 200 OK
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint
 // We have no .await call, so it is not needed anymore.
@@ -15,6 +20,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("subscriptions", web::post().to(subscribe))
         })
         .listen(listener)?
         .run();
