470d90df99b86c68581d0ff9135c31a28ea83f5a
Shahrullo <shahrullo777@gmail.com>
2024-05-18 16:09:33 +0900

    updated with database connection testing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,9 @@
 //! tests/health_check.rs
-
 use std::net::TcpListener;
+use actix_web::web::get;
+use sqlx::{PgConnection, Connection};
+use email_newsletter::configurations::get_configuration;
 
-// No .await call, therefore no need for `spawn_app` to be async
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind address");
@@ -43,6 +44,11 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     // Act
@@ -57,6 +63,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscriptions.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
