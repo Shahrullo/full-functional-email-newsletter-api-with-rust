cb0492449c9810e0a6041b17dc64f646d9cce82a
Shahrullo <shahrullo777@gmail.com>
2024-05-18 15:33:55 +0900

    modified run function calling

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -8,7 +8,7 @@ fn spawn_app() -> String {
         .expect("Failed to bind address");
     // Retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
-    let server = email_newsletter::run(listener).expect("Failed to bind address");
+    let server = email_newsletter::startup::run(listener).expect("Failed to bind address");
     // Launch the server as background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
