cda0a7f2285f57300b6be4ce107d388509dd5a57
Shahrullo <shahrullo777@gmail.com>
2024-05-18 15:47:05 +0900

    modified to read configuration as its first step

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 //! src/main.rs
 use std::net::TcpListener;
 use email_newsletter::startup::run;
+use email_newsletter::configurations::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    // Panic if cannot read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
