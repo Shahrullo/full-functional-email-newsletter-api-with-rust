779365bb77f376849227cd6bf250a73e2088921b
Shahrullo <shahrullo777@gmail.com>
2024-05-19 10:06:59 +0900

    registered PgConnection as part of the application state

--- src/startup.rs
+++ src/startup.rs
@@ -2,13 +2,21 @@ use crate::routes::{health_check, subscribe};
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
 use std::net::TcpListener;
+use sqlx::PgConnection;
 
 // We need to mark `run` as public.
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    connection: PgConnection
+) -> Result<Server, std::io::Error> {
+    // wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("subscriptions", web::post().to(subscribe))
+            // Get a pointer copy and attach it to the application state
+            .app_data(connection.clone())
         })
         .listen(listener)?
         .run();
