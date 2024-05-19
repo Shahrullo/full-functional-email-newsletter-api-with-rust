8a5ce8d334c36219c8a49d12be8ce73d1b44aa2f
Shahrullo <shahrullo777@gmail.com>
2024-05-19 10:07:52 +0900

    included pgconnection to the main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 //! src/main.rs
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use email_newsletter::startup::run;
 use email_newsletter::configurations::get_configuration;
+
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Panic if cannot read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
