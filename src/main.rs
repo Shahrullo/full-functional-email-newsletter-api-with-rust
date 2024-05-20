db4a4ba10ef46bef17fbc7bb7c3a619ad0b414b0
SHAHRULLOHON KHURSHEDKHO LUTFILLOHONOV <shahrullo@laivdata.com>
2024-05-20 17:00:37 +0900

    printing all logs at info-level or above

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 //! src/main.rs
 use sqlx::PgPool;
+use env_logger::Env;
 use std::net::TcpListener;
 use email_newsletter::startup::run;
 use email_newsletter::configurations::get_configuration;
 
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // printing all logs at info-level or above
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Panic if cannot read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
