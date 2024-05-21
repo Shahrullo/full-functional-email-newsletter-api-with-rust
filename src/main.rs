73dbe9b3813e32e0f41e4ca87e716e8343bd9650
SHAHRULLOHON KHURSHEDKHO LUTFILLOHONOV <shahrullo777@gmail.com>
2024-05-21 14:05:24 +0900

    refactored according to telemetry logging

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,15 @@
 //! src/main.rs
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use tracing_log::LogTracer;
+use email_newsletter::telemetry::{get_subscriber, init_subscriber};
 use email_newsletter::startup::run;
 use email_newsletter::configurations::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // redirect all `log`s events to the subcriber
-    LogTracer::init().expect("Failed to set logger");
-    
-    // printing all logs at info-level or above
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "email_newsletter".into(),
-        std::io::stdout
-    );
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    // call the subscriber
+    let subscriber = get_subscriber("email_newsletter".into(), "info".into());
+    init_subscriber(subscriber);
 
     // Panic if cannot read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
