01241b30d7e264f1324397810b4f4ba32c9f7678
SHAHRULLOHON KHURSHEDKHO LUTFILLOHONOV <shahrullo777@gmail.com>
2024-05-21 13:46:32 +0900

    extra log formatter is added and tracing log is initialized for all logs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 //! src/main.rs
 use sqlx::PgPool;
-use env_logger::Env;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 use email_newsletter::startup::run;
 use email_newsletter::configurations::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // redirect all `log`s events to the subcriber
+    LogTracer::init().expect("Failed to set logger");
+    
     // printing all logs at info-level or above
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "email_newsletter".into(),
+        std::io::stdout
+    );
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     // Panic if cannot read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
