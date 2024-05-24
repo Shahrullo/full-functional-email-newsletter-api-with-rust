2d8678af79bb6e4b830d23ea3095043686bc9c7c
Shahrullo <shahrullo777@gmail.com>
2024-05-24 18:58:50 +0900

    main logic is udpated with new configurations

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //! src/main.rs
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use email_newsletter::telemetry::{get_subscriber, init_subscriber};
@@ -13,9 +14,8 @@ async fn main() -> std::io::Result<()> {
 
     // Panic if cannot read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-        // .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
+    
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
