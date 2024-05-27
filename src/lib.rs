b69cd82654a8e7e3b2936c72843502787a223244
SHAHRULLOHON KHURSHEDKHO LUTFILLOHONOV <shahrullo777@gmail.com>
2024-05-27 16:00:32 +0900

    updated with new email client api logic accordingly

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 //! src/lib.rs
 
-pub mod configurations;
 pub mod domain;
 pub mod routes;
 pub mod startup;
 pub mod telemetry;
+pub mod email_client;
+pub mod configurations;
--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,21 @@
-use crate::routes::{health_check, subscribe};
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
+use crate::email_client::{self, EmailClient};
+use crate::routes::{health_check, subscribe};
+
 // We need to mark `run` as public.
 pub fn run(
     listener: TcpListener,
-    db_pool: PgPool
+    db_pool: PgPool,
+    email_client: EmailClient,
 ) -> Result<Server, std::io::Error> {
     // wrap the connection in a smart pointer
     let db_pool = web::Data::new(db_pool);
+    let email_client = web::Data::new(email_client);
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
@@ -19,6 +23,7 @@ pub fn run(
             .route("subscriptions", web::post().to(subscribe))
             // Get a pointer copy and attach it to the application state
             .app_data(db_pool.clone())
+            .app_data(email_client.clone())
         })
         .listen(listener)?
         .run();
