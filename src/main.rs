c63cad8cb837e2159d7fb8d670969e0cd58e5d40
SHAHRULLOHON KHURSHEDKHO LUTFILLOHONOV <shahrullo777@gmail.com>
2024-05-27 15:59:01 +0900

    a bug is fixed

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> std::io::Result<()> {
     // Panic if cannot read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
+        // .connect_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
     
     // Build an `EmailClient` using `configuration`
