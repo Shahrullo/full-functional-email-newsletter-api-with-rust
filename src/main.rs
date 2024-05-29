8e9465b92505e4c59456ac65e92b3a3207a876de
Shahrullo <shahrullo777@gmail.com>
2024-05-29 18:42:15 +0900

    sharing startup logic with server and tests. Updated codes accrodingly

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 //! src/main.rs
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
-use email_newsletter::telemetry::{get_subscriber, init_subscriber};
-use email_newsletter::startup::run;
+use email_newsletter::startup::Application;
 use email_newsletter::configurations::get_configuration;
-use email_newsletter::email_client::EmailClient;
+use email_newsletter::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -14,23 +11,9 @@ async fn main() -> std::io::Result<()> {
 
     // Panic if cannot read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new()
-        // .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
     
-    // Build an `EmailClient` using `configuration`
-    let sender_email = configuration.email_client.sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url, 
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
 
-    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client)?.await?;
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
     Ok(())
 }
