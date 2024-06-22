ea9b05e4d579e92e4e05761992bd4516681ec3a1
Shahrullo <shahrullo777@gmail.com>
2024-06-22 21:28:18 +0900

    a redis session store instance is created and connected

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use email_newsletter::configurations::get_configuration;
 use email_newsletter::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     // call the subscriber
     let subscriber = get_subscriber("email_newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
