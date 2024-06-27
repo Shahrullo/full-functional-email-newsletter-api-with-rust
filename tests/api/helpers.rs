a21dc464d8011f45dcbd3cf748f3b225d3ac757f
Shahrullo <shahrullo777@gmail.com>
2024-06-27 21:26:44 +0900

    a migration is included

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -21,7 +21,8 @@ impl TestUser {
         Self {
             user_id: Uuid::new_v4(),
             username: Uuid::new_v4().to_string(),
-            password: Uuid::new_v4().to_string()
+            // password: Uuid::new_v4().to_string()
+            password: "everythinghastostartsomewhere".into(),
         }
     }
 
@@ -35,6 +36,7 @@ impl TestUser {
             .hash_password(self.password.as_bytes(), &salt)
             .unwrap()
             .to_string();
+        dbg!(&password_hash);
         // let password_hash = format!("{:x}", password_hash);
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash)
